d6a4d5dbe2243626303a6d1fe61ab68587e00380
breno.polanski <breno.polanski@gmail.com>
2016-03-14 22:55:07 -0300

    Add gulp tasks

---
+++ gulp/tasks/jade.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// Necessary Plugins
+var gulp    = require('gulp');
+var plumber = require('gulp-plumber');
+var fs      = require('fs');
+var jade    = require('jade');
+var paths   = require('../paths');
+
+// Call Jade to compile templates
+module.exports = gulp.task('jade', function() {
+  return gulp.src(paths.source.files.jade)
+    .pipe(plumber())
+    .pipe(jade({
+      pretty: true
+    }))
+    .pipe(gulp.dest(paths.build.html));
+});
